0e0e12bf31ef647b2a4f217c4d64212a57829408
Suresh D <suresh.nashi10@gmail.com>
2026-03-17 12:07:26 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages";
+
 export default function App() {
   return (
-    <div className="p-10">
-      <h1 className="text-5xl text-red-500 font-bold">
-        Tailwind Working 🚀
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}>
+</Route>
+    </Routes>
   );
 }
