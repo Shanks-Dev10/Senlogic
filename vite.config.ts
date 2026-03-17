e604df165cc79d8f2b3823431c0b2d6315a7968b
Suresh D <suresh.nashi10@gmail.com>
2026-03-17 18:46:04 +0530

    changes in vite congifg

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: "postcss", // ✅ ADD THIS LINE
+  },
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
