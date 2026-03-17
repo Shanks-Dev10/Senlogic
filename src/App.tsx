39b7d91e3e95f34df24c3cf35fe2ce2ea6b0cef0
Suresh D <suresh.nashi10@gmail.com>
2026-03-17 11:44:12 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl text-red-500 font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
